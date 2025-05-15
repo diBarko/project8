3ffbf70dc2f73514a86a6624b6bd03cadf913bf7
Denis Barko <denisbarko@hotmail.com>
2025-05-15 19:57:55 +0300

    feat: add hello world

---
+++ src/gr/aueb/cf/helloapp/HelloWorld.java
@@ -0,0 +1,14 @@
+package gr.aueb.cf.helloapp;
+
+/**
+ * This project was created to show how
+ * to make a git project,
+ * how to stage and commit changes in local repo,
+ * and finally, how to push to GitHub.
+ * */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello GitHub!");
+    }
+}
